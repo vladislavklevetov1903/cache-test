59305cd071a4d90bc8d510705f4f92c93fb69cfc
Vladislav Klevetov <kvd1131@yandex.ru>
2024-07-08 12:39:36 +0300

    Create swagger package

--- src/main/java/com/example/lesson8/Swagger/SwaggerConfig.java
+++ src/main/java/com/example/lesson8/Swagger/SwaggerConfig.java
@@ -1,17 +1,11 @@
-package com.example.lesson8;
+package com.example.lesson8.Swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @Configuration
